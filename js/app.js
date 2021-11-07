2ac34bca9f0479df5263aaa19d799190dc79e373
alfattahdicky <alfattahdicky2@gmail.com>
2021-11-08 02:54:08 +0700

    finish project html & css

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburgerEl = document.getElementById('hamburger');
-  const closeEl = document.getElementById('close');
-  const navListEl = document.getElementById('navList');
+const hamburgerEl = document.getElementById('hamburger');
+const closeEl = document.getElementById('close');
+const navListEl = document.getElementById('navList');
+const notificationEl = document.getElementById('notifikasi');
+const btnNotificationEl = document.getElementById('btnNotif');
 
-  hamburgerEl.addEventListener('click', function () {
-    navListEl.classList.add('open');
-  });
+hamburgerEl.addEventListener('click', function () {
+  navListEl.classList.add('open');
+});
 
-  closeEl.addEventListener('click', function () {
-    navListEl.classList.remove('open');
-  });
+closeEl.addEventListener('click', function () {
+  navListEl.classList.remove('open');
+});
+
+btnNotificationEl.addEventListener('click', function (e) {
+  if (notificationEl.classList.contains('open')) {
+    notificationEl.classList.remove('open');
+  } else {
+    notificationEl.classList.add('open');
+  }
 });
